1d0162bc2d5fe9cf012ffab876f27b24adeaf27f
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-13 19:08:32 +0200

    DB Config

--- database/db_config.php
+++ database/db_config.php
@@ -1,9 +1,9 @@
 <?php //db_config.php
 
 // DEFINE DATABASE VARIABLES
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_HOST", "localhost");
-define("DB_NAME", "pulsationaudio_tools");
+define("DB_USER", "u96278");
+define("DB_PASS", "EjBcRppUgCpXYRYW");
+define("DB_HOST", "mysql10.manitu.net");
+define("DB_NAME", "db96278");
 
 define("DB_CONFIG", TRUE);
