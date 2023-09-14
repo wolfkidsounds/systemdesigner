e25f6b6f1926313158e70d64f9f57be91b03e98f
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-14 22:00:29 +0200

    Include Toolbar CSS

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="/includes/assets/css/container.css">
 <link rel="stylesheet" href="/includes/assets/css/overwrites.css">
+<link rel="stylesheet" href="/includes/assets/css/toolbar.css">
 <?php //container.php
 
 if ($navigation) { require_once __DIR__ . "/navigation.php"; }
