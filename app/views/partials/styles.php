c092c53eec7b7c31fd56ec9cea7648a84a429b87
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-14 21:48:21 +0200

    Implement FontAwesome

--- app/views/partials/styles.php
+++ app/views/partials/styles.php
@@ -1,2 +1,4 @@
-<link rel="stylesheet" href="/node_modules\@fortawesome\fontawesome-free\css\all.css">
-<link rel="stylesheet" href="/node_modules\spectre.css\dist\spectre.min.css">   
+<link rel="stylesheet" href="/node_modules\spectre.css\dist\spectre.min.css">
+<link href="/node_modules\@fortawesome\fontawesome-free\css\fontawesome.css" rel="stylesheet">
+<link href="/node_modules\@fortawesome\fontawesome-free\css\brands.css" rel="stylesheet">
+<link href="/node_modules\@fortawesome\fontawesome-free\css\solid.css" rel="stylesheet">
