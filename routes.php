65edd33702ae266d9cff688f535b83b82e870589
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-14 21:47:11 +0200

    New Routes

--- routes.php
+++ routes.php
@@ -16,6 +16,16 @@ get('/', function () { Modules::Views()->Index(); });
 get('/login', function () { Modules::Views()->Login(); });
 get('/logout', function () { Modules::Views()->Logout(); });
 
+get('/app', function () { Modules::Views()->App_Dashboard(); });
 get('/app/dashboard', function () { Modules::Views()->App_Dashboard(); });
+get('/app/amplifiers', function () { Modules::Views()->App_Amplifiers(); });
+get('/app/speakers', function () { Modules::Views()->App_Speakers(); });
+get('/app/dsp', function () { Modules::Views()->App_DSP(); });
+get('/app/setups', function () { Modules::Views()->App_Setups(); });
+get('/app/configurations', function () { Modules::Views()->App_Configurations(); });
+get('/app/management', function () { Modules::Views()->App_Management(); });
+
+get('/user/account', function () { Modules::Views()->User_Account(); });
+get('/user/settings', function () { Modules::Views()->User_Settings(); });
 
 any('/404', 'app/views/404.php');
