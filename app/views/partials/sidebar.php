dcdbc69704ed67efe62a4d291e18e9bb4dd97e55
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-14 21:48:11 +0200

    Sidebar

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -1,2 +1,27 @@
 <link rel="stylesheet" href="/includes/assets/css/sidebar.css">
 <?php //sidebar.php
+?>
+
+<sidebar>
+    <ul class="nav">
+        <li class="nav-item">
+            <a href="/app/amplifiers">Amplifiers</a>
+        </li>
+        <li class="nav-item">
+            <a href="/app/speakers">Speakers</a>
+        </li>
+        <li class="nav-item">
+            <a href="/app/dsp">DSP & Processor</a>
+        </li>
+        <div class="horizontal-divider"></div>
+        <li class="nav-item">
+            <a href="/app/setups">Setups</a>
+        </li>
+        <li class="nav-item">
+            <a href="/app/configurations">Configuration</a>
+        </li>
+        <li class="nav-item">
+            <a href="/app/management">Management</a>
+        </li>
+    </ul>
+</sidebar>
