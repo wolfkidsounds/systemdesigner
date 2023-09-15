f79d37c088e429900d08f0b8cd0b19e8349bdd60
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-16 00:04:24 +0200

    new functions and documentation

--- app/functions.php
+++ app/functions.php
@@ -3,7 +3,14 @@
 require_once ABSPATH . "database/database.php";
 
 class Functions {
+    /**
+     * Checks if a user is logged in
+     *
+     * @return true if the user is logged in, false otherwise.
+     */
     public static function checkLogin() {
+        Functions::startSession();
+
         if (isset($_SESSION['user_id']) && isset($_SESSION['user_name']) && isset($_SESSION['user_mail'])) {
             return true;
         }   
@@ -15,11 +22,20 @@ class Functions {
         return false;
     }
 
+    /**
+     * Regisers a user into the database
+     *
+     * @param [string] $name
+     * @param [string] $mail
+     * @param [string] $password
+     * @return true if the user is registered successfully, false otherwise
+     */
     public static function registerUser($name, $mail, $password) {
+        Functions::startSession();
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
         $db = new Database();
-        $query = "INSERT INTO user (user_name, user_mail, user_password) VALUES (?, ?, ?)";
+        $query = "INSERT INTO user (user_name, user_mail, user_pass) VALUES (?, ?, ?)";
         $result = $db->query($query, $name, $mail, $hashed_password);
         
         if ($result) {
@@ -31,7 +47,14 @@ class Functions {
         return false;
     }
 
+    /**
+     * Checks if a user already exists
+     *
+     * @param [type] $mail
+     * @return true if a user exists, false otherwise
+     */
     public static function checkUser($mail) {
+        Functions::startSession();
         $db = new Database();
         $query = "SELECT COUNT(*) AS count FROM user WHERE user_mail = ?";
         $exists = $db->query($query, $mail)->fetchArray()['count'] > 0;
@@ -44,22 +67,112 @@ class Functions {
         return false;
     }
 
+    /**
+     * Logs in a user
+     *
+     * @param [type] $mail
+     * @param [type] $password
+     * @return void if the user was logged in succesfully, false otherwise
+     */
     public static function loginUser($mail, $password) {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        
+        Functions::startSession();
+       
         $db = new Database();
-        $query = "SELECT user_id, user_name, password FROM user WHERE user_mail = ?";
+        $query = "SELECT user, user_name, user_mail, user_pass FROM user WHERE user_mail = ?";
         $user = $db->query($query, $mail)->fetchArray();
 
-        if ($user) {
-            if (password_verify($password, $user['user_password'])) {
-                session_start();
-                $_SESSION['user_id'] = $user['user_id'];
-                $_SESSION['user_name'] = $user['user_name'];
-                $_SESSION['user_mail'] = $mail['user_mail'];
-                $db->close();
-                return true;
-            }
+        if ($user && password_verify($password, $user['user_pass'])) {
+            session_regenerate_id(true);
+            $_SESSION['user_id'] = $user['user'];
+            $_SESSION['user_name'] = $user['user_name'];
+            $_SESSION['user_mail'] = $user['user_mail'];
+            $db->close();
+            header("Location: /dashboard");
+            exit();
+        }
+    
+        $db->close();
+        return false;        
+    }
+
+    /**
+     * Starts a session safely
+     *
+     * @return void
+     */
+    public static function startSession() {
+
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    /**
+     * Logs out the user
+     *
+     * @return void
+     */
+    public static function logoutUser() {
+        Functions::startSession();
+        $_SESSION = [];
+        session_destroy();
+        session_regenerate_id(true);
+        header("Location: /login");
+        exit();
+    }
+
+    /**
+     * Checks if fields are empty
+     *
+     * @param [type] $key
+     * @return true if fields are set, false otherwise
+     */
+    public static function checkEmptyFields($key) {
+        if (isset($key)) {
+            return true;
+        }   
+
+        if (!empty($key)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if a Brand Name already exists
+     *
+     * @param [type] $brand_name
+     * @return true if already exists, false otherwise
+     */
+    public static function checkBrand($brand_name) {
+        $db = new Database();
+        $query = "SELECT COUNT(*) AS count FROM brands WHERE brand_name = ?";
+        $exists = $db->query($query, $brand_name)->fetchArray()['count'] > 0;
+        
+        if ($exists) {
+            $db->close();
+            return true;
+        }
+        $db->close();
+        return false;
+    }
+    /**
+     * Registers a new brand
+     *
+     * @param [type] $brand_name
+     * @return true if successfully registered, false otherwise
+     */
+    public static function registerBrand($brand_name) {
+        Functions::startSession();
+
+        $db = new Database();
+        $query = "INSERT INTO brands (brand_name, user_id) VALUES (?, ?)";
+        $result = $db->query($query, $brand_name, $_SESSION["user_id"]);
+        
+        if ($result) {
+            $db->close();
+            return true;
         }
         
         $db->close();
