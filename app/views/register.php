49d40fd789c194509a02478018567431fe30b2fb
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-16 00:05:29 +0200

    New Register View

--- app/views/register.php
+++ app/views/register.php
@@ -2,7 +2,6 @@
 <link rel="stylesheet" href="includes/assets/css/register.css">
 <?php //register.php
 
-session_start();
 require_once ABSPATH . "/app/functions.php";
 if (Functions::checkLogin()) {
     header("Location: /app/dashboard");
@@ -21,15 +20,15 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     if (empty($name) || empty($mail) || empty($password)) {
         ?> 
-        <div class="toast error">
+        <div class="toast toast-error">
             <p>The Fields can not be empty.</p>
         </div>
         <?php
     }
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         ?> 
-        <div class="toast error">
+        <div class="toast toast-error">
             <p>Please type a valid email address.</p>
         </div>
         <?php
@@ -37,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     if (preg_match('/^[a-zA-Z0-9 _-]+$/', $name)) {
         ?> 
-        <div class="toast error">
+        <div class="toast toast-error">
             <p>Please type a valid name.</p>
         </div>
         <?php
@@ -45,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     if (preg_match('/^[a-zA-Z0-9_-]+$/', $password)) {
         ?> 
-        <div class="toast error">
+        <div class="toast toast-error">
             <p>Please type a valid password.</p>
         </div>
         <?php
@@ -53,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     if (Functions::checkUser($mail)) {
         ?> 
-        <div class="toast error">
+        <div class="toast toast-error">
             <p>The email was already registered.</p>
         </div>
         <?php
@@ -67,16 +66,16 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     } else {
         ?> 
-        <div class="toast error">
+        <div class="toast toast-error">
             <p>Something went wrong during registration.</p>
         </div>
 
         <h3>Register</h3>
-        <form name="Register" method="post">
+        <form name="Register" method="post" action="/register">
         <div class="form-group">
             <div class="register-form">
                 <input class="form-input" type="name" id="name" name="name" value="<?php out($name) ?>" placeholder="Name">
-                <input class="form-input" type="email" id="email" name="mail" value="<?php out($email) ?>" placeholder="E-Mail">
+                <input class="form-input" type="email" id="mail" name="mail" value="<?php out($mail) ?>" placeholder="E-Mail">
                 <input class="form-input" type="password" id="password" name="password" placeholder="Password">
                 <button class="btn btn-primary input-group-btn">Register</button>
             </div>
@@ -92,11 +91,11 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     ?>
 
     <h3>Register</h3>
-    <form name="Register" method="post">
+    <form name="Register" method="post" action="/register">
     <div class="form-group">
         <div class="register-form">
         <input class="form-input" type="name" id="name" name="name" placeholder="Name">
-                <input class="form-input" type="email" id="email" name="mail" placeholder="E-Mail">
+                <input class="form-input" type="email" id="mail" name="mail" placeholder="E-Mail">
                 <input class="form-input" type="password" id="password" name="password" placeholder="Password">
             <button class="btn btn-primary input-group-btn">Register</button>
         </div>
