d25e586bbdf5b305cd30347468df967bd5539684
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-16 00:07:54 +0200

    Add Brand View Remove Logincheck rename dsp to processor

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -1,5 +1,11 @@
 <link rel="stylesheet" href="/includes/assets/css/sidebar.css">
 <?php //sidebar.php
+
+require_once ABSPATH . "/app/functions.php";
+if (!Functions::checkLogin()) {
+    header("Location: /login");
+    exit();
+}
 ?>
 
 <sidebar>
@@ -11,7 +17,7 @@
             <a href="/app/speakers">Speakers</a>
         </li>
         <li class="nav-item">
-            <a href="/app/dsp">DSP & Processor</a>
+            <a href="/app/processors">Processors (DSP)</a>
         </li>
         <div class="horizontal-divider"></div>
         <li class="nav-item">
@@ -22,6 +28,10 @@
         </li>
         <li class="nav-item">
             <a href="/app/management">Management</a>
+        </li>
+        <div class="horizontal-divider"></div>
+        <li class="nav-item">
+            <a href="/app/brands">Brands</a>
         </li>
     </ul>
 </sidebar>
