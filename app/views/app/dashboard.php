1d194ea15b7ccae0cc378ee9ae4975b3dc00750f
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-16 00:06:24 +0200

    Remove Logincheck

--- app/views/app/dashboard.php
+++ app/views/app/dashboard.php
@@ -1,14 +1,6 @@
 <link rel="stylesheet" href="includes/assets/css/dashboard.css">
 <?php //dashboard.php
 
-
-// session_start();
-// require_once ABSPATH . "/app/functions.php";
-// if (!Functions::CheckUserLoggedIn()) {
-//     header("Location: /");
-//     exit();
-// }
-
 require_once VIEWSPATH . "partials/inc_partials.php";
 Partials::Open();
 Partials::Header(true, true);
