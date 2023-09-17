73b7ac10099173e7568156135ec47de51970d174
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-17 21:49:22 +0200

    Website Title

--- app/views/partials/open.php
+++ app/views/partials/open.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pulsation Audio - System Designer</title>
     <link rel="stylesheet" href="/node_modules/spectre.css/dist/spectre.min.css">
     <link href="/node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css" rel="stylesheet">
     <link href="/node_modules/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
