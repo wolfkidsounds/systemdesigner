78ad4dfb6a4cb0d7b20c773766eb8216b9226024
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-18 23:48:59 +0200

    (WIP) Translations

---
+++ app/lang/de.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang = array(
+    'login' => 'Anmelden',
+    'amplifier' => 'Verstärker',
+    'amplifiers' => 'Verstärker'
+);
+
+return $lang;
---
+++ app/lang/en.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang = array(
+    'login' => 'Login',
+    'amplifier' => 'Amplifier',
+    'amplifiers' => 'Amplifiers'
+);
+
+return $lang;
