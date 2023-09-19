a13ae7361c909b52d89660b8bedff9ec4fdd9fb9
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-19 16:52:47 +0200

    Update close.php

--- app/views/partials/close.php
+++ app/views/partials/close.php
@@ -2,5 +2,17 @@
 
 <?php require_once __DIR__ . "/scripts.php" ?>
 
+<!-- Language Selector
+<div class="language-selector">
+    <form method="post" action="/app/language">
+        <select name="selected_language">
+            <option value="en"><?php Translator::translate("language.english"); ?></option>
+            <option value="de"><?php Translator::translate("language.german"); ?></option>
+        </select>
+        <button type="submit"><?php Translator::translate("language.select"); ?></button>
+    </form>
+</div>
+-->
+
 </body>
 </html>
