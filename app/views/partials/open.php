48477545dab693ca7eae57eb6ad34b87cc9a417c
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-21 00:55:38 +0200

    Update open.php

--- app/views/partials/open.php
+++ app/views/partials/open.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" href="/includes/assets/css/overwrites.css">
     <link rel="stylesheet" href="/includes/assets/css/toolbar.css">
     <link rel="stylesheet" href="/includes\assets\css\chips.css">
-    <link href="/node_modules\select2\dist\css\select2.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="/includes\assets\css\toast.css">
+    <link rel="stylesheet" href="/node_modules\select2\dist\css\select2.min.css">    
   </head>
 
   <body>
