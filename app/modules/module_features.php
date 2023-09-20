393053bed16ec7217c3e5c7f60e8b6e9dd5cdd48
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-20 04:44:53 +0200

    Create module_features.php

---
+++ app/modules/module_features.php
@@ -0,0 +1,35 @@
+<?php
+
+class Features {
+    public static function getSpeakerFeature() {
+        return false;
+    }
+    public static function getAmplifierFeature() {
+        return false;
+    }
+    public static function getBrandFeature() {
+        return false;
+    }
+    public static function getChassisFeature() {
+        return false;
+    }
+    public static function getRacksFeature() {
+        return false;
+    }
+    public static function getConfigurationFeature() {
+        return false;
+    }
+    public static function getManagementFeature() {
+        return false;
+    }
+    public static function getDownloadsFeature() {
+        return false;
+    }
+    public static function getUserAccountFeature() {
+        return false;
+    }
+    public static function getUserRegisterFeature() {
+        return false;
+    }
+
+}
