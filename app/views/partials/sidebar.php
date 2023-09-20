7ab678c7ba25ab8886935e6d1af417a101231398
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-21 00:55:42 +0200

    Update sidebar.php

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -10,33 +10,64 @@ if (!Functions::Users()->checkLogin()) {
 
 <sidebar>
     <ul class="nav">
-        <li class="nav-item">
-            <a href="/app/amplifiers"><?php Translator::translate("sidebar.amplifiers"); ?></a>
-        </li>
-        <div class="horizontal-divider"></div>
-        <li class="nav-item">
-            <a href="/app/speakers"><?php Translator::translate("sidebar.speakers"); ?></a>
-        </li>
-        <li class="nav-item">
-            <a href="/app/chassis"><?php Translator::translate("sidebar.chassis"); ?></a>
-        </li>
-        <div class="horizontal-divider"></div>
-        <li class="nav-item">
-            <a href="/app/processors"><?php Translator::translate("sidebar.processors"); ?></a>
-        </li>
-        <div class="horizontal-divider"></div>
+        <?php if (Modules::Features()->getBrandFeature()) { ?>
         <li class="nav-item">
             <a href="/app/brands"><?php Translator::translate("sidebar.brands"); ?></a>
         </li>
-        <div class="horizontal-divider"></div>
+        <?php } ?>
+
+        <?php if (Modules::Features()->getProcessorFeature()) { ?>
         <li class="nav-item">
-            <a href="/app/setups"><?php Translator::translate("sidebar.setups"); ?></a>
+            <a href="/app/processors"><?php Translator::translate("sidebar.processors"); ?></a>
         </li>
+        <?php } ?>
+
+
+        <?php if (Modules::Features()->getAmplifierFeature()) { ?>
+        <li class="nav-item">
+            <a href="/app/amplifiers"><?php Translator::translate("sidebar.amplifiers"); ?></a>
+        </li>
+        <?php } ?>
+
+        <?php if (Modules::Features()->getChassisFeature()) { ?>
+        <li class="nav-item">
+            <a href="/app/chassis"><?php Translator::translate("sidebar.chassis"); ?></a>
+        </li>
+        <?php } ?>
+
+        <?php if (Modules::Features()->getSpeakerFeature()) { ?>
+        <li class="nav-item">
+            <a href="/app/speakers"><?php Translator::translate("sidebar.speakers"); ?></a>
+        </li>
+        <?php } ?>
+
+        <div class="horizontal-divider"></div>        
+
+        <?php if (Modules::Features()->getLimiterFeature()) { ?>
+        <li class="nav-item">
+            <a href="/app/limiters"><?php Translator::translate("sidebar.limiters"); ?></a>
+        </li>
+        <?php } ?>
+
+        <div class="horizontal-divider"></div>
+
+        <?php if (Modules::Features()->getRackFeature()) { ?>
+        
+        <li class="nav-item">
+            <a href="/app/rack"><?php Translator::translate("sidebar.racks"); ?></a>
+        </li>
+        <?php } ?>
+
+        <?php if (Modules::Features()->getConfigurationFeature()) { ?>
         <li class="nav-item">
             <a href="/app/configurations"><?php Translator::translate("sidebar.configurations"); ?></a>
         </li>
+        <?php } ?>
+
+        <?php if (Modules::Features()->getManagementFeature()) { ?>
         <li class="nav-item">
             <a href="/app/management"><?php Translator::translate("sidebar.management"); ?></a>
         </li>
+        <?php } ?>
     </ul>
-</sidebar>
+</sidebar>  
