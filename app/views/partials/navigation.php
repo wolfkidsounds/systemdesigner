66ef3acfa52c28d7958ffcd1654d43712ecb822c
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-21 00:55:35 +0200

    Update navigation.php

--- app/views/partials/navigation.php
+++ app/views/partials/navigation.php
@@ -6,13 +6,21 @@
     <h5>Pulsation Audio - Designer</h5>
   </section>
   <section class="navbar-section">
-    <?php
-
-    require_once ABSPATH . "app/functions/functions.php";    
-    if (Functions::Users()->checkLogin()) { ?>
-
-    <a class="nav-account" href="/user/account" class="btn btn-link"><i class="fas fa-user"></i><?php out($_SESSION['user_name']) ?></a>
-      
+  <?php 
+  require_once ABSPATH . "app/functions/functions.php";    
+  if (Functions::Users()->checkLogin()) { ?>
+    <div class="dropdown">
+        <a class="btn btn-link btn-primary dropdown-toggle dropdown-right" tabindex="0">
+          <?php out($_SESSION['user_name']) ?> 
+          <i class="fa-solid fa-caret-down" style="color: #ffffff;"></i>
+        </a>
+        <ul class="menu">
+          <li class="menu-item"><a href="/user/account"><?php Translator::translate("navigation.account") ?></a></li>
+          <li class="menu-item"><a href="/user/settings"><?php Translator::translate("navigation.settings") ?></a></li>
+          <li class="menu-item"><a href="/app/version"><?php Translator::translate("app.version") ?></a></li>
+          <li class="menu-item"><a href="/logout"><?php Translator::translate("navigation.logout") ?></a></li>
+        </ul>
+    </div>
     <?php } ?>
   </section>
 </header>
