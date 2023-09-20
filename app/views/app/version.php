f678eac00c0c11a1778c9d33253b4c708a6d141e
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-21 00:55:32 +0200

    Update version.php

--- app/views/app/version.php
+++ app/views/app/version.php
@@ -1,4 +1,27 @@
-<?php //version.php
-Translator::translate("version.current_version");
-out("alpha-0.0.1");
+<?php //amplifier.php
+
+require_once VIEWSPATH . "partials/inc_partials.php";
+Partials::Open();
+Partials::Header(true, true);
+
+$file_path = 'changelog.txt';
+
+if (file_exists($file_path)) {
+    $file_contents = file_get_contents($file_path);
+} else {
+    $file_contents = 'File not found.';
+}
 ?>
+
+<h3><?php Translator::translate("app.version"); ?></h3>
+<div class="form-divider">
+    <p><?php out(VERSION); ?></p>
+</div>
+<div class="form-divider">
+    <h6><?php Translator::translate("app.changelog"); ?></h6>
+    <pre>
+        <?php echo htmlspecialchars($file_contents); ?>
+    </pre>
+</div>
+
+<?php Partials::Close(); ?>
