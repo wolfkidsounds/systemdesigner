9a040aab163643d72966c37b13e71b9f3283c308
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-21 22:28:51 +0200

    Update speakers.php

--- app/views/app/speakers.php
+++ app/views/app/speakers.php
@@ -1,5 +1,4 @@
-<link rel="stylesheet" href="includes/assets/css/speaker.css">
-<?php //speaker.php
+<?php //speakers.php
 
 require_once VIEWSPATH . "partials/inc_partials.php";
 Partials::Open();
@@ -11,14 +10,10 @@ Partials::Header(true, true);
 <div class="toolbar">
     <ul>
         <li><a href="/app/new/speaker"><?php Translator::translate("speakers.new_speaker"); ?></a></li>
-    </ul>
-</div>
-
-<div class="toolbar-search">
-    <ul>
         <li><input class="form-input table-custom-search" type="search" id="search" placeholder="<?php Translator::translate("speakers.search"); ?>..."></li>
     </ul>
 </div>
+
 
 <div class="table-custom">
     <table class="table">
@@ -26,10 +21,11 @@ Partials::Header(true, true);
         <tr>
             <th><?php Translator::translate("speakers.brand"); ?></th>
             <th><?php Translator::translate("speakers.model"); ?></th>
+            <th><?php Translator::translate("speakers.type"); ?></th>
             <th>AES/RMS <?php Translator::translate("speakers.power"); ?> (W)</th>
             <th>Z nom. (Ω)</th>
             <th>Vrms (V)</th>
-            <th><?php Translator::translate("speakers.sensitivity"); ?> 1W @ 1m (dB SPL)</th>
+            <th>1W @ 1m (dB SPL)</th>
             <th><?php Translator::translate("speakers.contributors"); ?></th>
             <th><?php Translator::translate("speakers.actions"); ?></th>
         </tr>
@@ -37,23 +33,29 @@ Partials::Header(true, true);
     <tbody>
         <?php 
             
-            $speakers = Functions::Speakers()->getAllSpeakers();
+            $speakers = Functions::Speakers()->getAll();
 
             foreach ($speakers as $speaker) { ?>
-                <tr>
+                <tr data-id="<?php out($speaker["id"]); ?>">
                     <td>
                         <?php
-                            $brand_id = $speaker["speaker_brand"];
-                            $brand = Functions::Brands()->getBrand($brand_id);
-                            $brand_name = $brand["brand_name"];
-                            out($brand_name); 
+                            $brand_id = $speaker["brand_id"];
+                            $brand = Functions::Brands()->get($brand_id);
+                            if ($brand) {
+                                $brand_name = $brand["name"];
+                                out($brand_name);
+                            } else {
+                                out(Translator::translate("brands.not_found"));
+                            }
+                             
                         ?>
                     </td>
-                    <td><?php out($speaker["speaker_model"]); ?></td>
-                    <td><?php out($speaker["speaker_power_rms"]); ?></td>
-                    <td><?php out($speaker["speaker_impedance_z"]); ?></td>
-                    <td><?php out($speaker["speaker_vrms"]); ?></td>
-                    <td><?php out($speaker["speaker_sens_spl"]); ?></td>
+                    <td><?php out($speaker["name"]); ?></td>
+                    <td><?php out(Translator::translateReturn("speakers.type_" . $speaker["sp_type"])); ?></td>
+                    <td><?php out($speaker["power_rms"]); ?></td>
+                    <td><?php out($speaker["impedance"]); ?></td>
+                    <td><?php out($speaker["vrms"]); ?></td>
+                    <td><?php out($speaker["sensitivity"]); ?></td>
                     <td>
                         <?php
                             $user_id = $speaker["user_id"];
@@ -64,8 +66,7 @@ Partials::Header(true, true);
                     </td>
                     <td>
                         <a class="edit action-button tooltip" data-tooltip="<?php Translator::translate("speakers.edit"); ?>" href="/app/edit/speaker/<?php out($speaker["id"]); ?>"><i class="fa-solid fa-pen"></i></a>
-                        <a class="del action-button tooltip" data-tooltip="<?php Translator::translate("speakers.delete"); ?>" href="/app/del/speaker/<?php out($speaker["id"]); ?>"><i class="fas fa-trash"></i></a>
-                        <a class="download action-button tooltip" data-tooltip="<?php Translator::translate("speakers.download_datasheet"); ?>" href="/app/download/speaker/<?php out($speaker["id"]); ?>"><i class="fas fa-file-download"></i></a>
+                        <a class="del action-button tooltip" data-id="<?php out($speaker["id"]); ?>" data-tooltip="<?php Translator::translate("speakers.delete"); ?>" href="javascript:void(0);" onclick="deleteItem('speaker', <?php out($speaker['id']); ?>);"><i class="fas fa-trash"></i></a>
                     </td>
                 </tr>
 
@@ -75,15 +76,7 @@ Partials::Header(true, true);
 </div>
 
 <script src="/node_modules\jquery\dist\jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-    $("#search").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $(".table tbody tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});
-</script>
+<script src="/includes\assets\js\overviews\search.js"></script>
+<script src="/includes\assets\js\overviews\delete_item.js"></script>
 
 <?php Partials::Close(); ?>
