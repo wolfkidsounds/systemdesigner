f1dfad4e32675ab882d4f6ec40a21378b39a7024
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-21 19:24:01 +0200

    clean the routes

--- routes.php
+++ routes.php
@@ -52,15 +52,6 @@ if (Modules::Features()->getBrandFeature()) {
   post('/app/del/brand/$brand_id', function ($brand_id) { Functions::Brands()->delete($brand_id); });
 }
 
-//Amplifier Routes
-if (Modules::Features()->getAmplifierFeature()) {
-  get('/app/amplifiers', function () { Modules::Views()->App_Amplifiers(); });
-  get('/app/new/amplifier', function () { Modules::Views()->App_New_Amplifier(); });
-  post('/app/new/amplifier', function () { Modules::Views()->App_New_Amplifier(); });
-  get('/app/edit/amplifier/$amplifier_id', function ($amplifier_id) { Modules::Views()->App_Edit_Amplifier($amplifier_id); });
-  post('/app/edit/amplifier/$amplifier_id', function ($amplifier_id) { Modules::Views()->App_Edit_Amplifier($amplifier_id); });
-}
-
 //Processor Routes
 if (Modules::Features()->getProcessorFeature()) {
   get('/app/processors', function () { Modules::Views()->App_Processors(); });
@@ -71,6 +62,16 @@ if (Modules::Features()->getProcessorFeature()) {
   post('/app/del/processor/$processor_id', function ($processor_id) { Functions::Processors()->delete($processor_id); });
 }
 
+//Amplifier Routes
+if (Modules::Features()->getAmplifierFeature()) {
+  get('/app/amplifiers', function () { Modules::Views()->App_Amplifiers(); });
+  get('/app/new/amplifier', function () { Modules::Views()->App_New_Amplifier(); });
+  post('/app/new/amplifier', function () { Modules::Views()->App_New_Amplifier(); });
+  get('/app/edit/amplifier/$amplifier_id', function ($amplifier_id) { Modules::Views()->App_Edit_Amplifier($amplifier_id); });
+  post('/app/edit/amplifier/$amplifier_id', function ($amplifier_id) { Modules::Views()->App_Edit_Amplifier($amplifier_id); });
+  post('/app/del/amplifier/$amplifier_id', function ($amplifier_id) { Functions::Amplifiers()->delete($amplifier_id); });
+}
+
 //Speaker Routes
 if (Modules::Features()->getSpeakerFeature()) {
   get('/app/speakers', function () { Modules::Views()->App_Speakers(); });
@@ -78,6 +79,7 @@ if (Modules::Features()->getSpeakerFeature()) {
   post('/app/new/speaker', function () { Modules::Views()->App_New_Speaker(); });
   get('/app/edit/speaker/$speaker_id', function ($speaker_id) { Modules::Views()->App_Edit_Speaker($speaker_id); });
   post('/app/edit/speaker/$speaker_id', function ($speaker_id) { Modules::Views()->App_Edit_Speaker($speaker_id); });
+  post('/app/del/speaker/$speaker_id', function ($speaker_id) { Functions::Amplifiers()->delete($speaker_id); });
 }
 
 //Racks Routes
