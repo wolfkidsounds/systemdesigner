ca1433cf372bf35ff74e57ae54cde7fd270b1fa0
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-30 20:38:53 +0200

    Update Builder.php

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -17,23 +17,6 @@ final class Builder implements ContainerAwareInterface
 
         $menu->addChild('Home', ['route' => 'homepage']);
 
-        // access services from the container!
-        $em = $this->container->get('doctrine')->getManager();
-        // findMostRecent and Blog are just imaginary examples
-        $blog = $em->getRepository(Blog::class)->findMostRecent();
-
-        $menu->addChild('Latest Blog Post', [
-            'route' => 'blog_show',
-            'routeParameters' => ['id' => $blog->getId()]
-        ]);
-
-        // create another menu item
-        $menu->addChild('About Me', ['route' => 'about']);
-        // you can also add sub levels to your menus as follows
-        $menu['About Me']->addChild('Edit profile', ['route' => 'edit_profile']);
-
-        // ... add more children
-
         return $menu;
     }
 
@@ -42,23 +25,6 @@ final class Builder implements ContainerAwareInterface
         $menu = $factory->createItem('root');
 
         $menu->addChild('Home', ['route' => 'homepage']);
-
-        // access services from the container!
-        $em = $this->container->get('doctrine')->getManager();
-        // findMostRecent and Blog are just imaginary examples
-        $blog = $em->getRepository(Blog::class)->findMostRecent();
-
-        $menu->addChild('Latest Blog Post', [
-            'route' => 'blog_show',
-            'routeParameters' => ['id' => $blog->getId()]
-        ]);
-
-        // create another menu item
-        $menu->addChild('About Me', ['route' => 'about']);
-        // you can also add sub levels to your menus as follows
-        $menu['About Me']->addChild('Edit profile', ['route' => 'edit_profile']);
-
-        // ... add more children
 
         return $menu;
     }
