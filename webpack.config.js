e38402bfde7dddfce027eaa619734e2d34d0034f
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-03 04:50:56 +0200

    Install Bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -67,7 +67,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
