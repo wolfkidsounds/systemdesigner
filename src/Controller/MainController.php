19c7544d14bc126e0b74d34f369c582e4cd746af
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-05 04:56:58 +0200

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(): Response
+    #[IsGranted('ROLE_USER')]
+    #[Route('/app', name: 'app_main')]
+    public function main(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-        ]);
+            'title' => 'Dashboard',
+            'iconclass' => 'icon-chassis',
+        ]);        
+    }
+
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        } else {
+            return $this->redirectToRoute('app_main');
+        }
     }
 }
