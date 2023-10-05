409908f004f2cf3a23c0146c77550eafc81bc494
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-05 04:57:39 +0200

    Create ProcessorType.php

---
+++ src/Form/ProcessorType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Processor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProcessorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Brand')
+            ->add('Name')
+            ->add('ChannelsInput')
+            ->add('ChannelsOutput')
+            ->add('OutputOffset')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Processor::class,
+        ]);
+    }
+}
