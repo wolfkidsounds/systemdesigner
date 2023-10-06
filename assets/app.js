7f33ccdf028306b09a5dd3a2b8f912e27fbb4b3a
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-06 12:07:13 +0200

    Update Styles

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,8 @@ import 'bootstrap';
 import 'simplebar';
 
 import './styles/app.scss';
-import './styles/app.css';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'balloon-css';
+
+import './styles/app.css';
