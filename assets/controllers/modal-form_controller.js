1f1d55f7ebd6e00944d104ec05f8118bfa701315
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-07 13:34:23 +0200

    EXPERIMENTAL - Modal

---
+++ assets/controllers/modal-form_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from '@hotwired/stimulus';
+import { Modal } from 'bootstrap';
+
+export default class extends Controller {
+    static targets = ['modal'];
+
+    openModal(event) {
+        const modal = new Modal(this.modalTarget);
+        modal.show();
+    }
+}
