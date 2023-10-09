89de7ccccba8734358f0b9300fb18335d7077892
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-09 11:56:42 +0200

    Create ManufacturerControllerTest.php

---
+++ tests/Controller/ManufacturerControllerTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Manufacturer;
+use App\Repository\ManufacturerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ManufacturerControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private ManufacturerRepository $repository;
+    private string $path = '/manufacturer/';
+    private EntityManagerInterface $manager;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Manufacturer::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Manufacturer index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'manufacturer[Name]' => 'Testing',
+            'manufacturer[User]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/manufacturer/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Manufacturer();
+        $fixture->setName('My Title');
+        $fixture->setUser('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Manufacturer');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Manufacturer();
+        $fixture->setName('My Title');
+        $fixture->setUser('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'manufacturer[Name]' => 'Something New',
+            'manufacturer[User]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/manufacturer/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getName());
+        self::assertSame('Something New', $fixture[0]->getUser());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Manufacturer();
+        $fixture->setName('My Title');
+        $fixture->setUser('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/manufacturer/');
+    }
+}
