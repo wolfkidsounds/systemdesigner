2361a9e052a7c68deee22d372c804476956692b6
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-10 07:11:45 +0200

    Update ProcessorType.php

--- src/Form/ProcessorType.php
+++ src/Form/ProcessorType.php
@@ -3,20 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Processor;
+use App\Entity\Manufacturer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ProcessorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Name')
-            ->add('Manufacturer')
-            ->add('ChannelsInput')
-            ->add('ChannelsOutput')
-            ->add('OutputOffset')
+            ->add('Manufacturer', EntityType::class, [
+                'class' => Manufacturer::class,
+                'choice_label' => 'name',
+                'constraints' => [new NotBlank()]
+            ])
+            ->add('Name', TextType::class, [
+                'constraints' => [new NotBlank()]
+            ])
+            ->add('ChannelsInput', IntegerType::class, [
+                'constraints' => [new NotBlank()]
+            ])
+            ->add('ChannelsOutput', IntegerType::class, [
+                'constraints' => [new NotBlank()]
+            ])
+            ->add('OutputOffset', IntegerType::class, [
+                'constraints' => [new NotBlank()]                
+            ])
         ;
     }
 
