c7bae2f6ff93cec7f3d9d046a34a5e4adce225f6
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-14 16:40:50 +0200

    Simplebar Implementation

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,19 @@
+//Import Node Modules
 import './bootstrap.js';
 import '@popperjs/core';
 import 'bootstrap';
+import 'simplebar';
 
+// Import Custom JS
 import './js/progress-circular.js';
 
+// Import SASS
 import './styles/app.scss';
 
+// Import CSS Modules
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'balloon-css';
+import 'simplebar/dist/simplebar.css';
 
+// Import custom CSS
 import './styles/app.css';
