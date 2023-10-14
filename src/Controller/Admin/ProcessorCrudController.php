31fdacadc2cf541454fff8e2af4c9a845f4732ce
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-14 15:23:50 +0200

    Update ProcessorCrudController.php

--- src/Controller/Admin/ProcessorCrudController.php
+++ src/Controller/Admin/ProcessorCrudController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Entity\Processor;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProcessorCrudController extends AbstractCrudController
 {
@@ -13,12 +20,21 @@ class ProcessorCrudController extends AbstractCrudController
         return Processor::class;
     }
 
-    public function configureFields(string $pageName): Iterable
+    public function configureFields(string $processor): iterable
     {
+        /** @var User $user */
+        $user = $this->getUser();
+        
         return [
-            IdField::new('id'),
-        ];
+            AssociationField::new('User')->setValue($user),
+            AssociationField::new('Manufacturer'),
+            TextField::new('name'),
+            IntegerField::new('channels_input'),
+            IntegerField::new('channels_output'),
+            IntegerField::new('output_offset'),
+            BooleanField::new('validated')
 
+        ];
     }
 
     /*
