f129049e917a21f085b8f0d17f6f0685adea9a0b
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-14 15:22:57 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,8 @@ import './bootstrap.js';
 import '@popperjs/core';
 import 'bootstrap';
 
+import './js/progress-circular.js';
+
 import './styles/app.scss';
 
 import '@fortawesome/fontawesome-free/css/all.css';
