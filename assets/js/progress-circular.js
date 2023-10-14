f913324d89588bb4d811cd07f3ff8a59f2b67534
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-14 15:23:02 +0200

    Create progress-circular.js

---
+++ assets/js/progress-circular.js
@@ -0,0 +1,13 @@
+// Get the circular progress bars
+const circularProgressBars = document.querySelectorAll(".progress-bar-circular");
+
+circularProgressBars.forEach(progressBar => {
+    const progress = parseFloat(progressBar.getAttribute("data-progress"));
+    const max = 10; // Set your max value here
+
+    // Calculate the angle for conic-gradient based on progress and max value
+    const angle = (progress / max) * 360;
+
+    // Update the background of the circular progress bar
+    progressBar.style.background = `conic-gradient(var(--bs-primary) ${angle}deg, #fff 0deg)`;
+});
