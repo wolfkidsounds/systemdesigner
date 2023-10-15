1f17815bd28e581a3eff1bda966cf90982629172
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-15 22:45:42 +0200

    Update AmplifierType.php

--- src/Form/AmplifierType.php
+++ src/Form/AmplifierType.php
@@ -7,8 +7,10 @@ use App\Entity\Manufacturer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
@@ -25,12 +27,39 @@ class AmplifierType extends AbstractType
             ->add('Name', TextType::class, [
                 'constraints' => [new NotBlank()]
             ])
-            ->add('Power16', IntegerType::class)
-            ->add('Power8', IntegerType::class)
-            ->add('Power4', IntegerType::class)
-            ->add('Power2', IntegerType::class)
-            ->add('PowerBridge8', IntegerType::class)
-            ->add('PowerBridge4', IntegerType::class)
+            ->add('Power16', IntegerType::class,[
+                'label' => 'Power @ 16Ω'
+            ])
+            ->add('Power8', IntegerType::class, [
+                'label' => 'Power @ 8Ω'
+            ])
+            ->add('Power4', IntegerType::class, [
+                'label' => 'Power @ 4Ω'
+            ])
+            ->add('Power2', IntegerType::class, [
+                'label' => 'Power @ 2Ω'
+            ])
+            ->add('PowerBridge8', IntegerType::class, [
+                'label' => 'Power Bridge @ 8Ω'
+            ])
+            ->add('PowerBridge4', IntegerType::class, [
+                'label' => 'Power Bridge @ 4Ω'
+            ])
+            ->add('Manual', FileType::class, [
+                'label' => 'Manual (PDF)',
+                'mapped' => false, // not associated with any entity
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10m',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'You can only upload PDF Files',
+                    ])
+                ],
+            ])
         ;
     }
 
