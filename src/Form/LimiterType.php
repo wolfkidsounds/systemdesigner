973df16f27d0d64b006f24882fa7d5b28b18e5d6
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-15 23:08:01 +0200

    Update LimiterType.php

--- src/Form/LimiterType.php
+++ src/Form/LimiterType.php
@@ -3,8 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Limiter;
+use App\Entity\Speaker;
+use App\Entity\Amplifier;
+use App\Entity\Processor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LimiterType extends AbstractType
@@ -12,10 +17,21 @@ class LimiterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('User')
-            ->add('Processor')
-            ->add('Amplifier')
-            ->add('Speaker')
+            ->add('Processor', EntityType::class, [
+                'class' => Processor::class,
+                'constraints' => [new NotBlank()],
+                'attr' => ['data-select' => 'true']
+            ])
+            ->add('Amplifier', EntityType::class, [
+                'class' => Amplifier::class,
+                'constraints' => [new NotBlank()],
+                'attr' => ['data-select' => 'true']
+            ])
+            ->add('Speaker', EntityType::class, [
+                'class' => Speaker::class,
+                'constraints' => [new NotBlank()],
+                'attr' => ['data-select' => 'true']
+            ])
         ;
     }
 
