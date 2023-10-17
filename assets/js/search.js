385ae8fddf2e81b47c116ac1c5348a63f0414187
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-17 22:30:08 +0200

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,11 @@
-$(document).ready(function() {
-    $('#search-input').on('input', function() {
-        var searchText = $(this).val().toLowerCase();
-        $('#search-table tr').each(function() {
-            var rowText = $(this).text().toLowerCase();
-            if (rowText.includes(searchText)) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
+$('#search-input').on('input', function() {
+    var searchText = $(this).val().toLowerCase();
+    $('#search-table tr').each(function() {
+        var rowText = $(this).text().toLowerCase();
+        if (rowText.includes(searchText)) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
     });
 });
