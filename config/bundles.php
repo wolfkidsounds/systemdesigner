9670885bbba1b69a0a38b0d6fd2feed0feb8ee09
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-17 21:11:41 +0200

    composer install pagerfanta

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
