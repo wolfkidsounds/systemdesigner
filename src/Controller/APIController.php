ec10bec37bb84cd645374ecb3bb0cd85f7eeed66
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-17 19:52:59 +0200

    Update APIController.php

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -10,9 +10,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Novaway\Bundle\FeatureFlagBundle\Annotation\Feature;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\DataTransformer\StringToFloatTransformer;
 
+#[Feature(name: "api")]
+#[IsGranted('ROLE_USER')]
 class APIController extends AbstractController
 {
     #[Route('/api/get/processor/{id}', name: 'api_get_processor')]
