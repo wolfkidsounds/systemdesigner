221f1108dcde0daab3a398eeb08cf483226f1c92
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-20 22:33:35 +0200

    Update LimiterType.php

--- src/Form/LimiterType.php
+++ src/Form/LimiterType.php
@@ -12,7 +12,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
 
 class LimiterType extends AbstractType
 {
@@ -22,49 +24,83 @@ class LimiterType extends AbstractType
             ->add('Processor', EntityType::class, [
                 'class' => Processor::class,
                 'constraints' => [new NotBlank()],
-                'attr' => ['data-select' => 'true']
+                'attr' => [ 'data-select' => 'true' ]
             ])
+
             ->add('Amplifier', EntityType::class, [
                 'class' => Amplifier::class,
                 'constraints' => [new NotBlank()],
                 'attr' => ['data-select' => 'true']
             ])
+
             ->add('Speaker', EntityType::class, [
                 'class' => Speaker::class,
                 'constraints' => [new NotBlank()],
                 'attr' => ['data-select' => 'true']
             ])
+
             ->add('Vrms', NumberType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('VrmsValue', NumberType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('VrmsAttack', TextType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('VrmsRelease', TextType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('Vpeak', NumberType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('VpeakValue', NumberType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('VpeakAttack', TextType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
             ])
+
             ->add('VpeakRelease', TextType::class, [
                 'constraints' => [new NotBlank()],
                 'attr' => ['readonly' => true]
+            ])
+
+            ->add('BridgeModeEnabled', ChoiceType::class, [
+                'label' => 'Bridge Mode Enabled',
+                'choices'  => [
+                    'Disabled' => false,
+                    'Enabled' => true,
+                ],
+            ])
+
+            ->add('InputSensitivity', NumberType::class, [
+                'label' => 'Input Sensitivity'
+            ])
+
+            ->add('SpeakersInParallel', NumberType::class, [
+                'label' => 'Speakers in Parallel'
+            ])
+
+            ->add('Scaling', PercentType::class, [
+                'label' => 'Limiter Scaling',
+                'attr' => [
+                    'min' => 1,
+                    'max' => 100,
+                ]
             ])
         ;
     }
