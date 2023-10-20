5c903408055ca6f0b59fbd367dae9883f4052910
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-20 13:56:06 +0200

    Update AmplifierControllerTest.php

--- tests/Controller/AmplifierControllerTest.php
+++ tests/Controller/AmplifierControllerTest.php
@@ -1,8 +1,9 @@
-<?php
+<?php // AmplifierControllerTest.php
 
 namespace App\Test\Controller;
 
 use App\Entity\Amplifier;
+use App\DataFixtures\UserFixtures;
 use App\Repository\AmplifierRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -19,10 +20,13 @@ class AmplifierControllerTest extends WebTestCase
     {
         $this->client = static::createClient();
         $this->repository = static::getContainer()->get('doctrine')->getRepository(Amplifier::class);
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
 
+        // Remove all existing Amplifier objects from the database
         foreach ($this->repository->findAll() as $object) {
             $this->manager->remove($object);
         }
+        $this->manager->flush();
     }
 
     public function testIndex(): void
@@ -32,39 +36,49 @@ class AmplifierControllerTest extends WebTestCase
         self::assertResponseStatusCodeSame(200);
         self::assertPageTitleContains('Amplifier index');
 
-        // Use the $crawler to perform additional assertions e.g.
-        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+        // Use the $crawler to perform additional assertions, if needed
     }
 
     public function testNew(): void
     {
+        $this->markTestIncomplete();
         $originalNumObjectsInRepository = count($this->repository->findAll());
 
-        $this->markTestIncomplete();
         $this->client->request('GET', sprintf('%snew', $this->path));
 
         self::assertResponseStatusCodeSame(200);
 
+        // Generate random values
+        $randomName = 'RandomName' . rand();
+        $randomPower16 = rand(1, 100); // Adjust the range as needed
+        $randomPower8 = rand(1, 100);
+        $randomPower4 = rand(1, 100);
+        $randomPower2 = rand(1, 100);
+        $randomPowerBridge8 = rand(1, 100);
+        $randomPowerBridge4 = rand(1, 100);
+
         $this->client->submitForm('Save', [
-            'amplifier[Name]' => 'Testing',
-            'amplifier[Power16]' => 'Testing',
-            'amplifier[Power8]' => 'Testing',
-            'amplifier[Power4]' => 'Testing',
-            'amplifier[Power2]' => 'Testing',
-            'amplifier[PowerBridge8]' => 'Testing',
-            'amplifier[PowerBridge4]' => 'Testing',
-            'amplifier[User]' => 'Testing',
-            'amplifier[Manufacturer]' => 'Testing',
+            'amplifier[Name]' => $randomName,
+            'amplifier[Power16]' => $randomPower16,
+            'amplifier[Power8]' => $randomPower8,
+            'amplifier[Power4]' => $randomPower4,
+            'amplifier[Power2]' => $randomPower2,
+            'amplifier[PowerBridge8]' => $randomPowerBridge8,
+            'amplifier[PowerBridge4]' => $randomPowerBridge4,
+            'amplifier[User]' => $user->getId(1), // Assuming User has an ID field
+            'amplifier[Manufacturer]' => $manufacturer->getId(1), // Assuming Manufacturer has an ID field
         ]);
 
         self::assertResponseRedirects('/amplifier/');
 
+        // Check that a new object is created in the repository
         self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
     }
 
     public function testShow(): void
     {
         $this->markTestIncomplete();
+
         $fixture = new Amplifier();
         $fixture->setName('My Title');
         $fixture->setPower16('My Title');
@@ -85,6 +99,7 @@ class AmplifierControllerTest extends WebTestCase
         self::assertPageTitleContains('Amplifier');
 
         // Use assertions to check that the properties are properly displayed.
+        self::assertSelectorTextContains('h1', 'My Title'); // Adjust the selector as needed
     }
 
     public function testEdit(): void
@@ -106,6 +121,8 @@ class AmplifierControllerTest extends WebTestCase
 
         $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
 
+        self::assertResponseStatusCodeSame(200);
+
         $this->client->submitForm('Update', [
             'amplifier[Name]' => 'Something New',
             'amplifier[Power16]' => 'Something New',
@@ -120,23 +137,22 @@ class AmplifierControllerTest extends WebTestCase
 
         self::assertResponseRedirects('/amplifier/');
 
-        $fixture = $this->repository->findAll();
+        $updatedFixture = $this->repository->find($fixture->getId());
 
-        self::assertSame('Something New', $fixture[0]->getName());
-        self::assertSame('Something New', $fixture[0]->getPower16());
-        self::assertSame('Something New', $fixture[0]->getPower8());
-        self::assertSame('Something New', $fixture[0]->getPower4());
-        self::assertSame('Something New', $fixture[0]->getPower2());
-        self::assertSame('Something New', $fixture[0]->getPowerBridge8());
-        self::assertSame('Something New', $fixture[0]->getPowerBridge4());
-        self::assertSame('Something New', $fixture[0]->getUser());
-        self::assertSame('Something New', $fixture[0]->getManufacturer());
+        self::assertSame('Something New', $updatedFixture->getName());
+        self::assertSame('Something New', $updatedFixture->getPower16());
+        self::assertSame('Something New', $updatedFixture->getPower8());
+        self::assertSame('Something New', $updatedFixture->getPower4());
+        self::assertSame('Something New', $updatedFixture->getPower2());
+        self::assertSame('Something New', $updatedFixture->getPowerBridge8());
+        self::assertSame('Something New', $updatedFixture->getPowerBridge4());
+        self::assertSame('Something New', $updatedFixture->getUser());
+        self::assertSame('Something New', $updatedFixture->getManufacturer());
     }
 
     public function testRemove(): void
     {
         $this->markTestIncomplete();
-
         $originalNumObjectsInRepository = count($this->repository->findAll());
 
         $fixture = new Amplifier();
@@ -153,12 +169,12 @@ class AmplifierControllerTest extends WebTestCase
         $this->manager->persist($fixture);
         $this->manager->flush();
 
-        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
-
         $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
         $this->client->submitForm('Delete');
 
-        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
         self::assertResponseRedirects('/amplifier/');
+
+        // Check that the object is removed from the repository
+        self::assertSame($originalNumObjectsInRepository - 1, count($this->repository->findAll()));
     }
 }
