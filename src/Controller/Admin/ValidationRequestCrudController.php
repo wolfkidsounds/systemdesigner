2fdabcb88acbb884d672517ff1cd6941240c91ad
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-23 18:59:32 +0200

    Create ValidationRequestCrudController.php

---
+++ src/Controller/Admin/ValidationRequestCrudController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\ValidationRequest;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ValidationRequestCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return ValidationRequest::class;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('User'),
+            TextField::new('Type'),
+            TextField::new('Object'),
+            ChoiceField::new('Status')
+                ->autocomplete()
+                ->renderAsBadges([
+                    'requested' => 'warning',
+                    'validated' => 'success',
+                    'rejected' => 'danger',
+                ])
+                ->setChoices([
+                    'Requested' => 'requested',
+                    'Validated' => 'validated',
+                    'Rejected' => 'rejected',
+                ]
+            ),
+            TextareaField::new('Message'),
+        ];
+        
+    }
+}
