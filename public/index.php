44e37dd7a6a70bdf99c9b08e265db654cba702a9
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-11-20 01:05:34 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,5 +7,11 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    if (!$context['APP_MAINTENANCE'] == false || !$context['APP_MAINTENANCE']) {
+        require_once 'index.html';
+    }
+
+    else {
+        return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    }
 };
