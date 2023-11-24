eafbd7063288ca996d5db6f64aad940b9c99f821
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-11-24 14:22:30 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -8,10 +8,7 @@ import 'select2/dist/js/select2.js';
 import 'datatables.net-buttons-bs5';
 import 'datatables.net-buttons/js/buttons.colVis.mjs';
 import 'datatables.net-buttons/js/buttons.html5.mjs';
-import 'datatables.net-fixedheader-bs5';
 import 'datatables.net-responsive-bs5';
-import 'datatables.net-rowgroup-bs5';
-import 'datatables.net-scroller-bs5';
 
 // Import CSS Modules
 import '@fortawesome/fontawesome-free/css/all.css';
