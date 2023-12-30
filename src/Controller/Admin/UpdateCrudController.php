1b553136575d51cae21b7dff73511f14e4619884
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-30 11:12:33 +0100

    Bugfixes and Improvements

--- src/Controller/Admin/UpdateCrudController.php
+++ src/Controller/Admin/UpdateCrudController.php
@@ -3,10 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Update;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UpdateCrudController extends AbstractCrudController
 {
@@ -15,14 +16,10 @@ class UpdateCrudController extends AbstractCrudController
         return Update::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('Name');
+        yield DateField::new('Date');
+        yield TextEditorField::new('Description');
     }
-    */
 }
