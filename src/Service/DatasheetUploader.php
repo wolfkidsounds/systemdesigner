14ef105810212e130d5afa4d0c8c5d3dab4bb74a
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-10 11:17:12 +0100

    Create DatasheetUploader.php

---
+++ src/Service/DatasheetUploader.php
@@ -0,0 +1,34 @@
+<?php // src/Service/DatasheetUploader.php
+namespace App\Service;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class DatasheetUploader
+{
+    public function __construct(
+        private string $targetDirectory,
+        private SluggerInterface $slugger,
+    ) {
+    }
+
+    public function upload(UploadedFile $file, string $manufacturer, string $name): string
+    {
+        $safeFilename = $this->slugger->slug($manufacturer . '-' . $name);
+        $fileName = $safeFilename.'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $fileName;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
+    }
+}
